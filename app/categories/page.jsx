adf5a9b233a9c3e91b40d25e0c69f05b95ae6479
Warren Moore <warren@2kingssolutions.com>
2024-06-21 22:10:54 +1000

    Styling done for search page, begun styling for category page

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import hamburger from "@/assets/hamburger.png";
 
 export default function Categories() {
   return (
     <section className="container">
       <h1>Pick a Category...</h1>
-      <a href="/results">
-        <div className="cards">
-          <Image></Image>
-          <h2>Beef</h2>
-        </div>
-      </a>
+      <div className="search-box">
+        <a href="/results">
+          <div className="cards">
+            <Image src={hamburger} width={100} alt="hamburger" />
+            <h2>Burgers</h2>
+          </div>
+        </a>
+      </div>
     </section>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,21 @@
+import Image from "next/image";
+import map from "@/assets/map.png";
+
 export default function HomePage() {
   const heading = "What's For Dinner";
   return (
-    <main>
+    <div className="container">
       <div className="search-box">
         <h1>{heading}</h1>
         <form action="">
           <label htmlFor="adsress">Enter your location...</label>
           <input type="text" name="address" id="address" />
-          <a href="/categories">Use Current Location</a>
+          <span className="loc-link">
+            <Image src={map} width={25} alt="map icon" />
+            <a href="/categories">Use Current Location</a>
+          </span>
         </form>
       </div>
-    </main>
+    </div>
   );
 }
