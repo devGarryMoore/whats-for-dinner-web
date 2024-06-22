a2139cfd08e30b935d59c7e6480c241b1bba63ab
Warren Moore <warren@2kingssolutions.com>
2024-06-22 12:33:04 +1000

    Initial styling of the website complete

--- app/results/page.jsx
+++ app/results/page.jsx
@@ -7,18 +7,23 @@ export default function ResultsPage() {
       <h1 className="heading">Results</h1>
       <div className="container">
         <div className="search-box">
-          <div>
-            <h2>Place name</h2>
-            <Image src={hamburger} width={200} alt="placeholder"/>
+          <div className="details">
+            <div>
+              <h2>Place name</h2>
+              <Image src={hamburger} width={200} alt="placeholder" />
+            </div>
+            <div>
+              <ul>
+                <li>Detail 1</li>
+                <li>Detail 2 etc</li>
+              </ul>
+            </div>
           </div>
+
           <div>
-            <ul>
-              <li>Detail 1</li>
-              <li>Detail 2 etc</li>
-            </ul>
+            <button className="btn transition-all">Try Again</button>
+            <button className="btn transition-all">Directions</button>
           </div>
-          <button>Try Again</button>
-          <button>Directions</button>
         </div>
       </div>
     </>
