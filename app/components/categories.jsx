5c4b60729671b536bcfe1c8ffc3b921eefd420d8
Warren Moore <warren@2kingssolutions.com>
2024-06-28 21:55:11 +1000

    categories page turned into component and page removed

--- app/components/categories.jsx
+++ app/components/categories.jsx
@@ -1,7 +1,68 @@
+import Image from "next/image";
+import hamburger from "@/assets/hamburger.png";
+import chicken from "@/assets/chicken.png";
+import pizza from "@/assets/pizza.png";
+import chinese from "@/assets/chinese.png";
+import pub from "@/assets/pub.png";
+import sushi from "@/assets/sushi.png";
+import korean from "@/assets/korean.png";
+import random from "@/assets/random.png";
+
 export default function Categories() {
   return (
     <div>
-      <h1>Categories</h1>
+      <h1 className="heading">Pick a category</h1>
+      <section className="container">
+        <div className="cards">
+          <a href="/results">
+            <Image src={hamburger} width={100} alt="hamburger" />
+            <h2>Burgers</h2>
+          </a>
+        </div>
+        <div className="cards">
+          <a href="/results">
+            <Image src={chicken} width={100} alt="chicken" />
+            <h2>Chicken</h2>
+          </a>
+        </div>
+        <div className="cards">
+          <a href="/results">
+            <Image src={pizza} width={100} alt="pizza" />
+            <h2>Pizza</h2>
+          </a>
+        </div>
+        <div className="cards">
+          <a href="/results">
+            <Image src={chinese} width={100} alt="chinese" />
+            <h2>Chinese</h2>
+          </a>
+        </div>
+        <div className="cards">
+          <a href="/results">
+            <Image src={pub} width={100} alt="pubs" />
+            <h2>Pubs</h2>
+          </a>
+        </div>
+        <div className="cards">
+          <a href="/results">
+            <Image src={sushi} width={100} alt="sushi" />
+            <h2>Sushi</h2>
+          </a>{" "}
+        </div>
+        <div className="cards">
+          {" "}
+          <a href="/results">
+            <Image src={korean} width={100} alt="korean" />
+            <h2>Korean</h2>
+          </a>{" "}
+        </div>
+        <div className="cards">
+          <a href="/results">
+            <Image src={random} width={100} alt="random" />
+            <h2>Random</h2>
+          </a>
+        </div>
+      </section>
     </div>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
+import Categories from "./components/categories";
 import Location from "./components/location";
 
 export default function HomePage() {
   return (
     <>
       <Location />
+      <Categories />
     </>
   );
 }
+
+//Todo
+//Have the categories component be hidden until the user gets their location
+//Hide the lcation component after the user gets their location
